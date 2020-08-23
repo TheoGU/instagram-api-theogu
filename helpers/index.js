501cfb12818c154aee50c6879c82d50d18c799e1
theo Guinebertier <dev@guinebertier.com>
2020-08-23 11:47:17 +0200

    Init

---
+++ helpers/index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  media: {
+    GraphImage: {
+      id: '1442533050805297981',
+      pk: 1442533050805297981,
+      shortcode: 'BQE6Cq2AqM9',
+      comments: [{ id: '17865614554065776' }]
+    }
+  },
+  users: {
+    Instagram: {
+      full_name: 'Instagram',
+      id: '25025320',
+      pk: 25025320,
+      username: 'instagram'
+    },
+    Maluma: {
+      full_name: 'MALUMA',
+      id: '44059601',
+      pk: 44059601,
+      username: 'maluma'
+    },
+    Xenia: {
+      full_name: 'XENIA',
+      id: '184028108',
+      pk: 184028108,
+      username: 'xenia'
+    }
+  },
+
+  locations: {
+    Santiago: {
+      id: '112371848779363',
+      pk: 26914683,
+      name: 'Santiago, Chile',
+      address: '',
+      city: '',
+      short_name: 'Santiago',
+      lng: -70.6667,
+      lat: -33.45
+    }
+  },
+  tags: {
+    dog: {
+      name: 'dog',
+      media_count: 151476906,
+      id: 17843640247048551
+    }
+  }
+}
